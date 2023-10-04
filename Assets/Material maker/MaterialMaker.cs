9f11086882a41d7bde432af58e402db97ba05434
ssamJang <74716771+colamong@users.noreply.github.com>
2023-10-04 22:37:54 +0900

    MaterialMaker
    MaterialMaker

--- Assets/Material maker/MaterialMaker.cs
+++ Assets/Material maker/MaterialMaker.cs
@@ -1,30 +1,89 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class MaterialMaker : MonoBehaviour
 {
     public Material material;
-    public GameObject Matball;
+    public GameObject matBall;
+    public RawImage previewImage;
 
+    public Slider sliderR;
+    public Slider sliderG;
+    public Slider sliderB;
+
+    public Color matColor;
+
+
+    private void Start()
+    {
+        material = matBall.GetComponent<MeshRenderer>().material;
+        material.SetColor("_Color",Color.white);
+        matColor = material.color;
+        sliderR.value = matColor.r;
+        sliderG.value = matColor.g;
+        sliderB.value = matColor.b;
+    }
+    private void Update()
+    {
+        if (material.GetColor("_Color").r != sliderR.value / 255)
+            ChangeColorR(sliderR);
+        if (material.GetColor("_Color").g != sliderG.value / 255)
+            ChangeColorR(sliderG);
+        if (material.GetColor("_Color").b != sliderB.value / 255)
+            ChangeColorR(sliderB);
+    }
     // Start is called before the first frame update
-    void Start()
+    public void GenerateMatball()
     {
-        
+        Instantiate(matBall, gameObject.transform.position + gameObject.transform.forward, Quaternion.identity);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CopyMaterial(Material mat)
     {
-        
+        matColor = material.color;
+        material.CopyPropertiesFromMaterial(mat);
+        material.color = matColor;
+        previewImage.material.CopyPropertiesFromMaterial(material);
     }
 
-    public void generateMat()
+    public void IncreaseValue(Slider Slider)
     {
-        Instantiate(Matball);
-        Instantiate(material);
-        Matball.GetComponent<Renderer>().material = material;
-        Matball.transform.position = gameObject.transform.position;
+        if(Slider.value < 255)
+            Slider.value+=1;
     }
 
+    public void decreaseValue(Slider Slider)
+    {
+        if (Slider.value > 0)
+            Slider.value-=1;
+    }
+
+    public void ChangeColorR(Slider Slider)
+    {
+        TextMeshProUGUI tmp = Slider.GetComponentInChildren<TextMeshProUGUI>();
+        matColor.r = Slider.value/255;
+        material.SetColor("_Color",matColor);
+        tmp.SetText(Slider.value.ToString());
+        previewImage.material.color = matColor;
+    }
+    public void ChangeColorG(Slider Slider)
+    {
+        TextMeshProUGUI tmp = Slider.GetComponentInChildren<TextMeshProUGUI>();
+        matColor.g = Slider.value/255;
+        material.SetColor("_Color", matColor);
+        tmp.SetText(Slider.value.ToString());
+        previewImage.material.color = matColor;
+    }
+    public void ChangeColorB(Slider Slider)
+    {
+        TextMeshProUGUI tmp = Slider.GetComponentInChildren<TextMeshProUGUI>();
+        matColor.b = Slider.value / 255;
+        material.SetColor("_Color", matColor);
+        tmp.SetText(Slider.value.ToString());
+        previewImage.material.color = matColor;
+    }
 }
